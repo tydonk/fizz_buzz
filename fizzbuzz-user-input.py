9a032afce46a91728c3b9e4507bbd81a8895912c
Tydon K <tydonk@gmail.com>
2016-06-10 05:27:06 +0000

    Current working version

--- fizzbuzz-user-input.py
+++ fizzbuzz-user-input.py
@@ -1,34 +1,29 @@
 import sys
 
-try:
-	if (len(sys.argv) > 1):
+cli_input = 0
+count_to = 0
+
+if (len(sys.argv) > 1):
+	try:
 		cli_input = int(sys.argv[1])
-		print("Fizz buzz counting up to " + str(cli_input) + "!")
-		for x in range(1,(cli_input + 1)):
-			if (x % 3 == 0) and (x % 5 == 0):
-				print("fizz buzz")
-			elif (x % 3 == 0) and (x % 5 != 0):
-				print("fizz")
-			elif (x % 5 == 0) and (x % 3 != 0):
-				print("buzz")
-			else:
-				print(x)
-	else:
+	except ValueError:
+		print("That is not a number, please run the game again and enter a number.")
+	print("Fizz buzz counting up to " + str(cli_input) + "!")
+	for x in range(1,(cli_input + 1)):
+		if (x % 3 == 0) and (x % 5 == 0):
+			print("fizz buzz")
+		elif (x % 3 == 0) and (x % 5 != 0):
+			print("fizz")
+		elif (x % 5 == 0) and (x % 3 != 0):
+			print("buzz")
+		else:
+			print(x)
+else:
+	try:
 		count_to = int(input("Please provide the number we will count to: "))
-		print("Fizz buzz counting up to {}!".format(count_to))
-		for x in range(1,(count_to + 1)):
-			if (x % 3 == 0) and (x % 5 == 0):
-				print("fizz buzz")
-			elif (x % 3 == 0) and (x % 5 != 0):
-				print("fizz")
-			elif (x % 5 == 0) and (x % 3 != 0):
-				print("buzz")
-			else:
-				print(x)
-except ValueError:
-	print("Sorry, that is not a valid input.  Please enter a number.")
-	count_to = int(input("Please provide the number we will count to: "))
-	print("Fizz buzz counting up to {}!".format(count_to))
+	except NameError:
+		print("That is not a number, please run the game again and enter a number.")
+	print("Fizz buzz counting up to " + str(count_to) + "!")
 	for x in range(1,(count_to + 1)):
 		if (x % 3 == 0) and (x % 5 == 0):
 			print("fizz buzz")
