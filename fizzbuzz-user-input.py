3aa0119de66d4c9374776e7145c82d4cb0e9ac19
Tydon K <tydonk@gmail.com>
2016-06-18 16:32:15 +0000

    Added exception to include Value Error for catching non integer inputs

--- fizzbuzz-user-input.py
+++ fizzbuzz-user-input.py
@@ -23,6 +23,8 @@ else:
 		count_to = int(input("Please provide the number we will count to: "))
 	except NameError:
 		print("That is not a number, please run the game again and enter a number.")
+	except ValueError:
+		print("That is not a number, please run the game again and enter a number.")
 	print("Fizz buzz counting up to " + str(count_to) + "!")
 	for x in range(1,(count_to + 1)):
 		if (x % 3 == 0) and (x % 5 == 0):
